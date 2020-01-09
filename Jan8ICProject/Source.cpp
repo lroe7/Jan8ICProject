16119cbbf2e74e1a21b8462e8715eb74545e675e
laure <laure@DESKTOP-HRJTM3L>
2020-01-09 10:07:19 -0700

    Finished part 1 and part 2

---
+++ Jan8ICProject/Source.cpp
@@ -0,0 +1,14 @@
+//Lauren Roe
+//This is my own work
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Enter a number in pounds \n" << endl;
+	double x;
+	cin >> x;
+	double convert = x * .454;
+	cout << x << " pounds = " << convert << " kilograms" << endl;
+	return 0;
+}
